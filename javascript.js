196271aad2a24106319f645589200d6d8707b061
M-Umair243 <engineerumair243@gmail.com>
2024-06-08 21:48:58 +0500

    initial commit

---
+++ javascript.js
@@ -0,0 +1,8 @@
+let Btn = document.getElementById("btn");
+let GifImg = document.getElementById("gif_Img");
+Btn.addEventListener("click", rainShow);
+function rainShow() {
+    GifImg.classList.toggle("showGif")
+    
+  
+}
